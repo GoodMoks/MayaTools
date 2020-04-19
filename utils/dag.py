52aac81b695c5ef75cf5a9ec1fb34152afda0d30
Max Morozov <goodmoks@gmail.com>
2020-04-19 16:50:51 +0300

    add docs
    add doc commects for 'dag' module and  for all functions

--- utils/dag.py
+++ utils/dag.py
@@ -2,7 +2,7 @@ import pymel.core as pm
 import maya.OpenMaya as om
 
 
-def history(node, type=None):
+def get_history(node, type=None):
     """ get node for type in history connections
 
     :param node: 'str' object
@@ -18,6 +18,11 @@ def history(node, type=None):
 
 
 def get_MObject(object):
+    """ get MObject for given object
+
+    :param object: 'str' object
+    :return: MObject
+    """
     selectionList = om.MSelectionList()
     om.MGlobal.getSelectionListByName(object, selectionList)
     mObject = om.MObject()
@@ -26,7 +31,11 @@ def get_MObject(object):
 
 
 def isShape(obj):
-    # Check Shape
+    """ check if the given object is a shape node
+
+    :param obj: 'str' object
+    :return: 'boot' True if object is shape
+    """
     mObject = get_MObject(obj)
     if not mObject.hasFn(om.MFn.kShape):
         return False
@@ -35,6 +44,13 @@ def isShape(obj):
 
 
 def get_children(obj, all=False, shapes=False):
+    """ get children for given object
+
+    :param obj: 'str' object
+    :param all: 'bool' True: get all children in hierarchy
+    :param shapes: 'bool' False: skip all shapes node
+    :return: 'list' of children or []
+    """
     child = pm.listRelatives(obj, c=True, ad=all)
     if child:
         if not shapes:
@@ -43,19 +59,11 @@ def get_children(obj, all=False, shapes=False):
 
 
 
-
-# def get_children(obj, all=False):
-#
-#     if child:
-#         child.append(obj)
-#         shapes_set = set()
-#         child_set = set(child)
-#         [[shapes_set.add(x) for x in pm.listRelatives(c, shapes=True)] for c in child if
-#          pm.listRelatives(c, shapes=True)]
-#         obj_set = set([obj])
-#         filtred_set = child_set.difference(shapes_set)
-#         return list(filtred_set.difference(obj_set))
-
-
 def get_parent(obj, all=False):
+    """ get parents for given objects
+
+    :param obj: 'str' object
+    :param all: 'bool' True: get all parents in hierarchy
+    :return: 'list' with parents or []
+    """
     return pm.listRelatives(obj, p=True, ap=all, shapes=False)
