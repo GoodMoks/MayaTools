3c72f0c0d06e4f3f2d9f29c0ec85bf3bc537d494
Max Morozov <goodmoks@gmail.com>
2020-04-19 20:47:47 +0300

    add core path module

---
+++ core/core_tools.py
@@ -0,0 +1,10 @@
+from ui.qtFloatSlider import *
+from ui.separator import *
+
+from attribute import *
+from connections import *
+from constraint import *
+from dag import *
+from particle import *
+from transform import *
+from vector import *
