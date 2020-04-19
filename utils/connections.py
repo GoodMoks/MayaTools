613fbe464d8d5eb96be8ebeb526ffd9fe7b867c5
Max Morozov <goodmoks@gmail.com>
2020-04-19 16:39:29 +0300

    modify 'connections' module
    made it possible to get common connections by attribute and without

--- utils/connections.py
+++ utils/connections.py
@@ -1,10 +1,21 @@
 import maya.cmds as cmds
 
 
-def get_common_connections(obj1, obj2, attr):
-    """ get common connections between two objects"""
-    connections = cmds.listConnections('{}.{}'.format(obj1, attr), d=True, p=True)
+def get_common_connections(obj1, obj2, attr=None):
+    """ get common connections for two objects
+    attribute "attr" allows get connections for current attribute
+
+    :param obj1: 'str' object
+    :param obj2: 'str' object
+    :param attr: 'attr' name of attribute ex: attr='worldMatrix'
+    :return: list with commons attr or []
+    """
+    obj = obj1
+    if attr:
+        obj = '{}.{}'.format(obj1, attr)
+    connections = cmds.listConnections(obj, d=True, p=True)
     if not connections:
         return None
 
     return [con.split('.')[1] for con in connections if con.split('.')[0] == obj2]
+
