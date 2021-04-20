af42eb528effcfedbb75636198278570ddea673a
Max Morozov <goodmoks@gmail.com>
2021-04-20 12:06:57 +0300

    style qtabwidget

--- tools/pipeline/menu_creator/menu_ui.py
+++ tools/pipeline/menu_creator/menu_ui.py
@@ -126,6 +126,95 @@ class TreeWidgetItemDelegate(QtWidgets.QStyledItemDelegate):
             return QtCore.QSize(0, self.item.ITEM_SIZE)
         return QtWidgets.QStyledItemDelegate.sizeHint(self, option, index)
 
+class CustomTabWidget(QtWidgets.QTabWidget):
+    def __init__(self, parent):
+        super(CustomTabWidget, self).__init__(parent)
+
+        #self.setDocumentMode(True)
+        self.setStyleSheet(""" 
+
+QTabWidget::pane {
+    border: 1px solid black;
+    
+}
+
+QTabWidget::tab-bar:top {
+    top: 1px;
+}
+
+QTabWidget::tab-bar:bottom {
+    bottom: 1px;
+}
+
+QTabWidget::tab-bar:left {
+    right: 1px;
+}
+
+QTabWidget::tab-bar:right {
+    left: 1px;
+}
+
+QTabBar::tab {
+    border: 2px solid black;
+}
+
+
+
+
+QTabBar::tab:top:!selected {
+    margin-top: 3px;
+}
+
+QTabBar::tab:bottom:!selected {
+    margin-bottom: 3px;
+}
+
+QTabBar::tab:top, QTabBar::tab:bottom {
+    min-width: 8ex;
+    margin-right: -1px;
+    padding: 5px 10px 5px 10px;
+}
+
+QTabBar::tab:top:selected {
+    border-bottom-color: none;
+}
+
+QTabBar::tab:bottom:selected {
+    border-top-color: none;
+}
+
+QTabBar::tab:top:last, QTabBar::tab:bottom:last,
+QTabBar::tab:top:only-one, QTabBar::tab:bottom:only-one {
+    margin-right: 0;
+}
+
+QTabBar::tab:left:!selected {
+    margin-right: 3px;
+}
+
+QTabBar::tab:right:!selected {
+    margin-left: 3px;
+}
+
+QTabBar::tab:left, QTabBar::tab:right {
+    min-height: 8ex;
+    margin-bottom: -1px;
+    padding: 10px 5px 10px 5px;
+}
+
+QTabBar::tab:left:selected {
+    border-left-color: none;
+}
+
+QTabBar::tab:right:selected {
+    border-right-color: none;
+}
+
+QTabBar::tab:left:last, QTabBar::tab:right:last,
+QTabBar::tab:left:only-one, QTabBar::tab:right:only-one {
+    margin-bottom: 0;
+}""")
+
 
 class MenuEditor(QtWidgets.QDialog):
     MAYA = pm.ui.PyUI('MayaWindow').asQtObject()
@@ -208,7 +297,7 @@ class MenuEditor(QtWidgets.QDialog):
         self.menu_combo = QtWidgets.QComboBox()
         self.menu_combo.addItem('Menu Name 1')
         self.menu_combo.addItem('Menu Name 1')
-        self.menu_combo.setEditable(True)
+        self.menu_combo.SelectedIndex = -1
         self.menu_combo.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
 
         # add menu button
@@ -265,15 +354,7 @@ class MenuEditor(QtWidgets.QDialog):
         self.option_box_editor = ScriptEditor(python=True)
 
         self.option_box_cb = QtWidgets.QCheckBox()
-        self.scripts_group_box = QtWidgets.QTabWidget()
-        self.scripts_group_box.setStyleSheet("""
-                                                QTabWidget::pane{
-                                                        border: none;
-                                                }
-                                                QTabBar::tab{
-                                                        min-width: 40ex;
-                                                }
-                                            """)
+        self.scripts_group_box = CustomTabWidget(self)
 
     def create_layout(self):
         self.main_ly = QtWidgets.QVBoxLayout(self)
